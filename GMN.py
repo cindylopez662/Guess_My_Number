a4b4b6209293187ef32fa0c6745fd00f13f9fde3
cindylopez662 <cindy.lopez705@gmail.com>
2020-09-14 22:58:01 -0400

    Update GMN.py

--- GMN.py
+++ GMN.py
@@ -1,3 +1,21 @@
+'''
+Notes:
+integer - int
+float - 1.0
+string - 'a string'
+index - kinda like a table of contents
+two loops: for and while 
+
+for <item> in [list]:
+    <do something>
+
+while loops:
+    while [something true]:
+        do something 
+        [break or true to false]
+
+    else statements, use true or false statements - break also works for false 
+'''
 my_number = 3
 is_incorrect_guess = True 
 while is_incorrect_guess:
