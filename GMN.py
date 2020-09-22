d081a1cb795e6f2085470f8a94195dfe8e365ac9
cindylopez662 <cindy.lopez705@gmail.com>
2020-09-21 23:42:03 -0400

    Update GMN.py

--- GMN.py
+++ GMN.py
@@ -16,12 +16,13 @@ while loops:
 
     else statements, use true or false statements - break also works for false 
 '''
-my_number = 3
-is_incorrect_guess = True 
-while is_incorrect_guess:
+import random
+magic_number = random.randrange(1,5)
+
+while True:
     guess = int(input('Guess my number from 1 to 5: '))
-    if guess == my_number:
+    if guess == magic_number:
         print("You got it right")
         break 
     else:
-        print("sorry try again") 
+        print("Sorry, try again") 
