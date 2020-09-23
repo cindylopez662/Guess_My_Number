387f8047797e56a406057290fbbca8744ca9d3f4
cindylopez662 <cindy.lopez705@gmail.com>
2020-09-22 20:27:39 -0400

    Update GMN.py

--- GMN.py
+++ GMN.py
@@ -5,15 +5,12 @@ float - 1.0
 string - 'a string'
 index - kinda like a table of contents
 two loops: for and while 
-
 for <item> in [list]:
     <do something>
-
 while loops:
     while [something true]:
         do something 
         [break or true to false]
-
     else statements, use true or false statements - break also works for false 
 '''
 import random
